6d577c5ed5e6ff2884c07af5fb0e9e1ca91062ba
JagoanRedesign <70496030+JagoanRedesign@users.noreply.github.com>
2024-10-23 17:42:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -85,4 +85,4 @@ def webhook():
 if __name__ == '__main__':
     # Set webhook
     bot.start()
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 8000)))
